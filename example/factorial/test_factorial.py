5a84f667eaae7da09dc674724bef808b7e86dcdd
Kenttsai1 <kent122277@gmail.com>
2022-09-25 14:06:47 +0800

    logtorial

--- example/factorial/test_factorial.py
+++ example/factorial/test_factorial.py
@@ -1,3 +1,7 @@
 from factorial import *
 def test_factorial():
     assert factorial(5) == 120
+
+def test_logFactorial():
+    assert factorial(5) == round(math.exp(logFactorial(5)))
+    # assert factorial(5) == math.exp(logFactorial(5))
